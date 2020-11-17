cc5ae9364b44ce24cfa059fa22d1731782acdec6
Danny Poizot <poizot.dan@gmail.com>
2020-11-17 14:18:02 +0100

    corrections css

--- js/scriptnav.js
+++ js/scriptnav.js
@@ -1,6 +1,7 @@
 function openNav(){
   document.getElementById("sidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("hero-image").style.marginLeft = "250px";
   document.getElementById("footer").style.marginLeft = "250px";
   document.getElementById("name").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -8,6 +9,7 @@ function openNav(){
 function closeNav(){
   document.getElementById("sidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("hero-image").style.marginLeft = "0";
   document.getElementById("footer").style.marginLeft = "0";
   document.getElementById("name").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
